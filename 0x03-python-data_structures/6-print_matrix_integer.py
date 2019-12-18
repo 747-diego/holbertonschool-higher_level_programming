aeea335ef3a21cff040fd1bc6cc744c6b14c6d98
Diego Tardio <diegotardio@Diegos-MBP.home>
2019-12-17 23:51:23 -0500

    [BUG FIX]

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # The join() method returns a string concatenated with the elements of an iterable.
     for index in matrix:
         print(' '.join('{:d}'.format(Number) for Number in index))
