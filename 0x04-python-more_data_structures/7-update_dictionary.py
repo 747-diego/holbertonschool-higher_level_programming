82b8df5843d89d7b9656eef7fd015b03577f5585
Diego Tardio <diegotardio@Diegos-MBP.home>
2019-12-19 23:43:02 -0500

    [PEP8 BUG FIX]

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# update function is used to add or remove keys in dictionary
+""" update function is used to add or remove keys in dictionary. """
+
+
 def update_dictionary(a_dictionary, key, value):
     a_dictionary.update({key: value})
     return a_dictionary
