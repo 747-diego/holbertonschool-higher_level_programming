01cf9f5c247e0651d4c247358c36239e4f122f9f
Diego Tardio <diegotardio@Diegos-MBP.home>
2019-12-19 23:27:16 -0500

    [TASK 8] Write a function that deletes a key in a dictionary.

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# .POP is a function used to delete keys inside this dictionary
+"""POP is a function used to delete keys inside this dictionary"""
+
+
 def simple_delete(a_dictionary, key=""):
     a_dictionary.pop(key, None)
     return a_dictionary
