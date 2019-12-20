d4bc9216b5fdb7f110ee5c594d5ba597e59209e5
Diego Tardio <diegotardio@Diegos-MBP.home>
2019-12-19 23:19:29 -0500

    [TASK 7] Write a function that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# update function is used to add or remove keys in dictionary
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary.update({key: value})
+    return a_dictionary
