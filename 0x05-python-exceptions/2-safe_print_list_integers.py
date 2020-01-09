6b7ccf8a4c737c0f9c0e0910fca84424a537a34a
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-08 23:58:35 -0500

    [TASK 2] BUGFIX index incrementation in wrong location

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     index = 0
     for iterator in range(0, x):
-        # index += 1
         try:
-            index += 1
+            # index += 1
             print("{:d}".format(my_list[iterator]), end='')
-        except(TypeError, ValueError):
+            index += 1
+        except(ValueError, TypeError):
             pass
     print()
     return index
