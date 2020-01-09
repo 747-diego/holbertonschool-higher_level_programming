4a36ff8f05722d27a816c9268c27c275dbe69bb5
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-08 23:35:53 -0500

    [BUGFIX] Added {d} in print format

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     index = 0
-    for iterator in range(x):
+    for iterator in range(0, x):
         # index += 1
         try:
             index += 1
             print("{:d}".format(my_list[iterator]), end='')
         except(TypeError, ValueError):
-            continue
+            pass
     print()
     return index
