cf90985eb0c9f9a06fdeee6307ddb0e26f331d9d
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-09 22:31:30 -0500

    [BUG FIX] SPELL CHECK

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,6 +5,6 @@ class Square:
         # Instantiation with size = 0
         self.__size = size
         if type(size) is not int:
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must ne >= 0")
+            raise ValueError("size must be >= 0")
