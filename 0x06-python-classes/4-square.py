e9f7e8d20fceea075510873d7331ac0de24f5a95
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-09 23:17:38 -0500

    [TASK 4] Write a class Square that defines a square by: (based on 3-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    # Creating Private instance attribute: size
+    def __init__(self, size=0):
+        # Instantiation with size = 0
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        # Public instance method: that returns the current square area
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        # property to retrieve it
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        # property setter to set it
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
