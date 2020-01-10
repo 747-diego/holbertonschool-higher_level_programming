4f3964d99a6cfe8106c8ac5ca38b2ff221325fbb
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-09 22:58:45 -0500

    [TASK 3] else if statment re-written

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
     def area(self):
         # Public instance method: that returns the current square area
