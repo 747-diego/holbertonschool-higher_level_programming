1c1abda3c5ca2c92ed1a7e3fac57ec0923ed63bc
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-09 22:41:36 -0500

    [TASK 3] Write a class Square that defines a square by: (based on 2-square.py)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    # Creating Private instance attribute: size
+    def __init__(self, size=0):
+        # Instantiation with size = 0
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    # Public instance method: that returns the current square area
+     def area(self):
+         return self.__self ** 2
