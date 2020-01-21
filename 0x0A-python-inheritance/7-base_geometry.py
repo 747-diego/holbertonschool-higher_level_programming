60f5e6c205a74d283b443b7233b5fef8e208b677
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-20 22:06:56 -0500

    [TASK 7] Write a class BaseGeometry with a public instance that validates values

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""TASK 7 Improved Geometry Module."""
+
+
+class BaseGeometry:
+    """Raise an implenmentation error: Area Exception."""
+
+    def area(self):
+        """Public instance."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance that validates value."""
+        if type(value) is not int:
+            raise TypeError(name + "must be an integer")
+        if value <= 0:
+            raise ValueError(name + "must be greater than 0")
