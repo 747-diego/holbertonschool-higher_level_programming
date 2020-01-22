cca92e8c70122ad72626e93c14a8f29bae7e8c7f
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-21 21:21:45 -0500

    [TASK 0] Write a function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr//bin/python3
+"""TASK 0 Read file."""
+
+
+def read_file(filename=""):
+    """Found function that reads a text file (UTF8) and prints it to stdout."""
+    with open(filename) as file:
+        print(file.read(), end="")
