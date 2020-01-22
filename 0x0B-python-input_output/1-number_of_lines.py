111d93152767f2c3c4d7834291359b40bf4222f0
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-21 22:36:10 -0500

    TASK 1] Write a function that returns the number of lines of a text file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -4,10 +4,10 @@
 
 def number_of_lines(filename=""):
     """Line Number function that returns the number of lines of a text file."""
-    if filename == "":
+    if filename is "":
         pass
     NumberOfLines = 0
     with open(filename, "r") as file:
-        for NumberOfLines in file:
+        for Lines in file:
             NumberOfLines += 1
     return NumberOfLines
