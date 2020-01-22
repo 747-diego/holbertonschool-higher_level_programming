06747fc21df09c20b9ca4e361e7f72da806e9431
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-21 21:50:57 -0500

    [TASK 0] Reading a file is independent of its encoding; you'll need to know the encoding in advance in order to properly interpret the bytes you read in.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
-#!/usr//bin/python3
+#!/usr/bin/python3
 """TASK 0 Read file."""
 
 
 def read_file(filename=""):
-    """Found function that reads a text file (UTF8) and prints it to stdout."""
+    """Read function that reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r") as file:
-        print(file.read(), end="")
+        text = file.read().decode("UTF8")
+        print(text, end="")
