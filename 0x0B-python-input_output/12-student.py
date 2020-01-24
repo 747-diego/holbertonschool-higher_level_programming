3f009ae6f359b6473287739f06e58c55efd0987d
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-01-24 12:22:27 -0500

    [TASK 12] Was able to print list but not by a specified attribute

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""TASK 11 Student to JSON."""
+
+
+class Student:
+    """Write a class Student that defines a student by."""
+
+    """Public instance attribute."""
+    def __init__(self, first_name, last_name, age):
+        """Instantiate Method."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method that retrieves a dictionary representation."""
+        """"Of a Student instance."""
+        ListOfAttrs = {}
+        if type(attrs) is list:
+            for index in range(len(attrs)):
+                if type(attrs[index]) is not str:
+                    return vars(self)
+        else:
+            for attribute, value in vars(self).items():
+                if type(attrs) is list:
+                    for index in attrs:
+                        if index is attribute:
+                            ListOfAttrs[attribute] = value
+        return vars(self)
