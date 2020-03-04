3c520824312f554275f0faa5c6f57b8b41ceba0f
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-03 23:05:24 -0500

    [TESTING]

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all shows, and all genres linked to that show
+-- from the database hbtn_0d_tvshows.
+SELECT title, name FROM tv_shows, tv_genres FROM title LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN name ON tv_show_genres.genre_id = tv_genres.id ORDER BY title, name ASC;
