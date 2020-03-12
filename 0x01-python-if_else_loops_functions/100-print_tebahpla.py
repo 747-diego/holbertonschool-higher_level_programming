d4376e0ff3b1b3a072401b0df5f2c0701f531b46
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-12 11:04:03 -0400

    [TESTING]

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 for letters in range(122, 96, -1):
-    print('{}'.format(chr(letters)), end="")
+    if letters % 2 == 1:
+        letters -= 32
+    print("{:c}".format(letters), end="")
