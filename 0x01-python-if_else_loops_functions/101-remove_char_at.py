9b6fcaab67e0c000c468104a981bfeaf907b4877
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-12 10:40:12 -0400

    [TESTING REMOVED STR]

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    """ Copy string and remove character """
+
+    NewString = ""
+    for index in range(0, len(str)):
+        if index != n:
+            NewString = NewString + NewString[index]
+    return NewString
