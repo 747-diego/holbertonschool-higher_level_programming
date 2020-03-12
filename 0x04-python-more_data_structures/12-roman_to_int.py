4468c0676a2542dcd6bff1f937feebd346563076
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-12 16:17:50 -0400

    [TESTING INDEX]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if (roman_string) is None or (isinstance(roman_string, str)) is False:
         return 0
-    RomeDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     RomeNum = 0
 
     for idx in range(len(roman_string)):
-        index = RomeDict[roman_string[idx]]
-        if idx < len(roman_string) - 1 and index < RomeDict[roman_string[idx+1]]:
+        index = rd[roman_string[idx]]
+        if idx < len(roman_string) - 1 and index < rd[roman_string[idx+1]]:
             RomeNum -= index
         else:
             RomeNum += index
