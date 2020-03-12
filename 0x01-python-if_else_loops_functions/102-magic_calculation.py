50b3f2157c98894bd58296ddd38698ea9ae93e15
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-12 11:34:04 -0400

    [TESTING]

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,11 +2,9 @@
 def magic_calculation(a, b):
 from magic_calculation_102 import add, sub
 
-    Num1 = a
-    Num2 = b
-    if Num1 < Num2:
-        outcome = add(Num1, Num2)
+    if a < b:
+        outcome = add(a, b)
         for index in range(4, 6):
             outcome = add(outcome, index)
         return outcome
-    return sub(Num1, Num2)
+    return sub(a, b)
