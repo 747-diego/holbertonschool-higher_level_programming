ff56a0fa9dca5ed6f9d64f42ff33fb3b7b72500b
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-12 18:42:37 -0400

    [TESTING KEYS]

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    KeysList = []
+    for keys, existing in a_dictionary.items():
+        if value is existing:
+            KeysList.append(keys)
+    if len(KeysList) > 0:
+        for SpecifiedValue in KeysList:
+            del a_dictionary[SpecifiedValue]
+    return a_dictionary
+    
