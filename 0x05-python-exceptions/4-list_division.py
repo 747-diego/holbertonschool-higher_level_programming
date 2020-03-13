1713cbe5d889c7965ba7667372d3e6b1ec29a91a
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-12 20:24:19 -0400

    [CHMOD]

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,10 +8,13 @@ def list_division(my_list_1, my_list_2, list_length):
             divs = my_list_1[index] / my_list_2[index]
         except ZeroDivisionError:
             print("division by 0")
+            divs = 0
         except TypeError:
             print("wrong type")
+            divs = 0
         except IndexError:
             print("out of range")
+            divs = 0
         finally:
             ElementList.append(divs)
             divs = 0
