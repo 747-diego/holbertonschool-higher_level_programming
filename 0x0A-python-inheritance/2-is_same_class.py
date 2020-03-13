0ee0365b804040d6ee55a93bc1e64884a13ce94d
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-12 20:15:26 -0400

    [CLEARING UP GIT]

--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""TASK 2  Exact same object."""
-
-
-def is_same_class(obj, a_class):
-    """Return True if object is exactly an instance; otherwise False."""
-    return issubclass(a_class, type(obj))
--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++
@@ -1,3 +0,0 @@
--- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
