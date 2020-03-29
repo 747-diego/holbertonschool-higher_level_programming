359f954f76c7dbed73e2570b51bc21fcdf3b893d
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-29 18:58:43 -0400

    [TEST] Testing fourth user argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a script that displays a sereched city."""
+if __name__ == "__main__":
+    from sys import argv as UserInput
+    import MySQLdb
+
+    MyName = UserInput[1]
+    MyPassword = UserInput[2]
+    MyDataBase = UserInput[3]
+    MyState = UserInput[4]
+
+    table = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=MyName,
+                            passwd=MyPassword,
+                            db=MyDataBase)
+    cursor = table.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for record in cursor.fetchall():
+        if record[1] == MyState:
+            print(record)
+    cursor.close()
+    table.close()
