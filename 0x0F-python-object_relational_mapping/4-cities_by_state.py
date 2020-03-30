5b2706254521e77a5349ddfbfda8557f14476f1b
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-29 20:26:13 -0400

    [TESTING] Test execute funcation

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a script that is safe from MySQL injections."""
+if __name__ == "__main__":
+    from sys import argv as UserInput
+    import MySQLdb
+
+    MyName = UserInput[1]
+    MyPassword = UserInput[2]
+    MyDataBase = UserInput[3]
+    MyState = UserInput[4]
+
+    table = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=MyName,
+                            passwd=MyPassword,
+                            db=MyDataBase)
+    cursor = table.cursor()
+    cursor.execute("SELECT * FROM cities\
+                   JOIN states ON cities.states_id = states.id\
+                   ORDER BY cities.id ASC")
+
+    for record in cursor.fetchall():
+        if record[1] == MyState:
+            print(record)
+
+    cursor.close()
+    table.close()
