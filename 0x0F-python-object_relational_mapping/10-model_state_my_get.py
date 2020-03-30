9037d4e1e48e2aec511f7fb574d98df53679984f
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-29 23:12:45 -0400

    [TESTING] checking if and else statement

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a script that prints the object with the name passed as argument."""
+if __name__ == "__main__":
+    from sys import argv as UserInput
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    MyName = UserInput[1]
+    MyPassword = UserInput[2]
+    MyDataBase = UserInput[3]
+    MyState = UserInput[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(MyName, MyPassword, MyDataBase))
+    Session = sessionmaker(engine)
+    session = Session()
+
+    for My in session.query(State).order_by(State.id):
+        if My.name == MyState:
+            print(My.id)
+        else:
+            print("Not Found")
