910e55c523a3e7bdc8eaaad9da66f1c1f90efc13
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-30 00:01:11 -0400

    [DELETE] Testing variables in session query

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write a script that deletes all State objects with letter a."""
+if __name__ == "__main__":
+    from sys import argv as UserInput
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    MyName = UserInput[1]
+    MyPassword = UserInput[2]
+    MyDataBase = UserInput[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(MyName, MyPassword, MyDataBase))
+    Session = sessionmaker(engine)
+    session = Session()
+
+    Letter = State.name.contains("a")
+    Syncro = (synchronize_session=False)
+    session.query(State).filter(Letter).delete(Syncro)
+    session.commit()
