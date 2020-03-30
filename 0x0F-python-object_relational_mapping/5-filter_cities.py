437cf4fcc998687b69da2b1cc0abfc1956596b45
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-29 21:58:33 -0400

    [CITY NAMES] Made empty array to hold the states city names

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a script that lists all cities from the database hbtn_0e_4_usa."""
+if __name__ == "__main__":
+    from sys import argv as UserInput
+    import MySQLdb
+
+    MyName = UserInput[1]
+    MyPassword = UserInput[2]
+    MyDataBase = UserInput[3]
+    MyState = UserInput[4]
+
+    table = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=MyName,
+                            passwd=MyPassword,
+                            db=MyDataBase)
+    cursor = table.cursor()
+    cursor.execute("SELECT cities.name\
+                   FROM cities\
+                   JOIN states ON cities.state_id = states.id\
+                   WHERE states.name = %s ORDER BY cities.id ASC", [MyState])
+
+    CityNames = []
+    for record in cursor.fetchall():
+        CityList.append(record[0])
+    print(", ".join(CityNames))
+
+    cursor.close()
+    table.close()
