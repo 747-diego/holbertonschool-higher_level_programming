5022ec148cea33d963b885ab51ba6ceb0c13af2c
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-29 23:49:59 -0400

    [TESTING SESSIONS]

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Write a script that changes the name of a object from the hbtn_0e_6_usa."""
+if __name__ == "__main__":
+    from sys import argv as UserInput
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    MyName = UserInput[1]
+    MyPassword = UserInput[2]
+    MyDataBase = UserInput[3]
+    Id = 2
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(MyName, MyPassword, MyDataBase))
+    Session = sessionmaker(engine)
+    session = Session()
+
+    session.query(State).filter(State.id == Id)
+    update.name = 'New Mexico'
+    session.commit()
