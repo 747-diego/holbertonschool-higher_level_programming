99ce611409303964ecb232f010a982754a3bc375
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-04-07 23:52:34 -0400

    [TEST]

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends GET request to the URL, display response
-curl -sH -XGET "$1" "X-HolbertonSchool-User-Id: 98"
+curl -s -XGET "$1" -H "X-HolbertonSchool-User-Id: 98"
