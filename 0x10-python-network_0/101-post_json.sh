9fe1ebcf57b1446de55265443c073e7803e9fba7
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-04-07 23:54:59 -0400

    [BUG FIX] Moved flags around

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a JSON POST request to a URL passed as the first argument
-curl -sH -XPOST "Content-Type: application/json" -d "@$2" "$1"
+curl -s -XPOST -H "Content-Type: application/json" -d "@$2" "$1"
