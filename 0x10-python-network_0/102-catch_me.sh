a261b8ed110172ea032fe47c916c9aab221db577
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-04-07 23:56:10 -0400

    [BUG FIX] Moved flags around

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to a server and responds with a message: 'You got me!'
-curl -sLH -XPUT "Origin: HolbertonSchool" -d "user_id=98"  0.0.0.0:5000/catch_me
+curl -sL -XPUT -H "Origin: HolbertonSchool" -d "user_id=98"  0.0.0.0:5000/catch_me
