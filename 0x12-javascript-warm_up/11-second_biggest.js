9c0672b0f135a8cabbc81c45900bad0f806e8fda
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-05 23:30:49 -0400

    [SEMI STANDARD BUG FIX]

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const BigNumTest = process.argv.slice(2);
 if (Numbers < 4) {
   console.log(0);
 } else {
-  const CheckTest = BigNumTest.sort(((a, b) => b - a));
+  const CheckTest = BigNumTest.sort((a, b) => b - a);
   console.log(CheckTest[1]);
 }
