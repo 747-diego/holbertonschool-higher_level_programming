a596d064fb19a3709ef1ef3286f9dfc7cc7cb851
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-05 23:29:49 -0400

    [TASK 11. Second biggest]

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Numbers = process.argv.length;
 const BigNumTest = process.argv.slice(2);
-if (Numbers <= 1) {
+if (Numbers < 4) {
   console.log(0);
 } else {
   const CheckTest = BigNumTest.sort(((a, b) => b - a));
