a5534f13f98cf6e0414f0ebc9135e85e764aa656
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-11 23:08:53 -0400

    [TASK 7. Occurrences]

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write a function that returns the number of occurrences in a list:
+exports.nbOccurences = function (list, searchElement) {
+    let Occurrences = 0;
+    for (let idx = 0; idx < list.length; idx++) {
+      if (list[idx] === searchElement {
+        Occurrences++;
+      }
+    }
+    return Occurrences;
+  };
