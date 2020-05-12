0aedef326bf62ed3452ff16b99d9bd6f163fa4ee
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-11 23:43:46 -0400

    [TESTING]

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // Write a script that imports an array and computes a new array.
-console.log(const Old = require('./100-data.js').list);
-console.log(const NewAray = Newaray.map((Value1, Value2) => Value1 * Value2));
+const Old = require('./100-data.js').list;
+console.log(Old);
+console.log(Old.map((Value1, Value2) => Value1 * Value2));
