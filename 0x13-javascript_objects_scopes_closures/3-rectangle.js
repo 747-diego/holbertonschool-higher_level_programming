809727d699c6bb317766fc9c6073cf6e2bb94e48
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-11 22:34:06 -0400

    [BUG FIX] Semi Standard checked for indentation

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 // Write a class Rectangle that defines a rectangle:
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-// instance method that prints the rectangle using the character X
-    print () {
-        for (let idx = 0; idx < this.height; idx++) {
-          console.log('X'.repeat(this.width));
-        }
-      }
-  };
+  }
+
+  // instance method that prints the rectangle using the character X
+  print () {
+    for (let idx = 0; idx < this.height; idx++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
