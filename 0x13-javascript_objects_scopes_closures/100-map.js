1b059d4784524f269f641e1f8dda55881ac1adf9
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-11 23:38:15 -0400

    [TESTING]

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Write a script that imports an array and computes a new array.
+console.log(require('./100-data.js').list);
+console.log(array.map((Value1, Value2) => Value1 * Value2));
