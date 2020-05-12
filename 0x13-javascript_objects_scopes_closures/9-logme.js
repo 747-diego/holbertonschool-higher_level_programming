325cf0d25e0871050024cc5123b93c1ceb6668c2
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-11 23:28:01 -0400

    [BUG FIX] Semi Standard detected trailing white space

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-// Write a function that prints the number of arguments 
-// already printed and the new argument value. 
+// Write a function that prints the number of arguments
+// already printed and the new argument value.
 let Arguments = 0;
 exports.logMe = function (item) {
   console.log(Arguments + ': ' + item);
