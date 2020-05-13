f5282b4ab293895c6f9308f1df178648c64960ce
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-12 23:51:53 -0400

    [TESTING]

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,7 +11,7 @@ GettingRequest(content[2], (err, res, body) => {
   const TasksCompleted = {};
   for (const tasks of JSON.parse(body)) {
     if (tasks.completed === true) {
-      let Id = tasks.userId;
+      const Id = tasks.userId;
       if (TasksCompleted) {
         TasksCompleted[Id] = TasksCompleted[Id] + 1;
       } else {
