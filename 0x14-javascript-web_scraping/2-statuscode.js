2227ecc5b9a182739c1d26c1e3ea5ad65eb3174e
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-12 22:27:51 -0400

    [TESTING]

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a script that display the status code of a GET request.
+const GettingRequest = require('request');
+const RequestingURL = process.argv[1];
+
+GettingRequest
+  .get(RequestingURL)
+  .on('response', (response) => {
+    console.log('code: ' + response.statusCode);
+  });
