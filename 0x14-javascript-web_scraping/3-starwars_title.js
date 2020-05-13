f9ea1760561e682169695cf549321a2594507ddf
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-12 22:40:33 -0400

    [TESTING]

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Write a script that prints the title of a Star Wars movie
+// where the episode number matches a given integer.
+
+const GettingRequest = require('request');
+const MovieID = process.argv[1];
+const MovieURL = 'http://swapi.co/api/films/' + MovieID;
+
+GettingRequest(MovieURL, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
