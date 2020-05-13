9d0d7edf9fafbfd33f5a388870840930d06d7804
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-12 22:53:46 -0400

    [TESTING]

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Write a script that gets the contents of a webpage and stores it in a file.
+const fs = require('fs');
+const GettingRequest = require('request');
+const content = process.argv;
+
+GettingRequest(content[2], (err, res, body) => {
+  if (error) {
+    console.log(error);
+  }
+
+  fs.writeFile(content[3], body, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
