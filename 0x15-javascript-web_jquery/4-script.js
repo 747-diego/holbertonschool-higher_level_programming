2526dd1e1db30216635fdc0cc1ad62a2114ecda1
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-15 11:53:13 -0400

    [TASK 4. Toggle classe]

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Write a Javascript script that toggles the class of the HTML tag HEADER
+// to red (#FF0000) when the user clicks on the tag DIV#toggle_header:
+$(function () {
+    $('DIV#toggle_header').click(function () {
+      $('header').toggleClass('FF0000 #008000');
+    });
+  });
