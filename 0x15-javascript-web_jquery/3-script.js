9ae4542c745193d5e7503c7098cfc67cd421840d
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-15 11:45:31 -0400

    [TASK 3. Add  class]

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Write a Javascript script that adds the class red
+// to the HTML tag HEADER to red (#FF0000)
+$('DIV#red_header').click(function () {
+    $('header').addClass('FF0000');
+  });
