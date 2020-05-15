bf584183d67257c531aad2ef4c2f5eec24652f5e
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-15 11:38:41 -0400

    [TASK 11. With jQuery API]

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Write a Javascript script that updates the text color of the HTML tag HEADER to red (#FF0000):
+// You must use the jQuery API
+$('header').css('color', '#FF0000');
