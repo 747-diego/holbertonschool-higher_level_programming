f221d5729378a65fc0550147ae8d1d2d307830d4
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-15 11:56:41 -0400

    [TASK 5. List of elements]

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+// #!/usr/bin/node
+// Write a Javascript script that adds a LI element
+// to a list when the user clicks on the tag DIV#add_item:
+// You can’t use document.querySelector to select the HTML tag
+$('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
